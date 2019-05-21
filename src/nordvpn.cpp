f9b98d74a5a3e24d9eb977e5b8b1290aa4ec1d28
alex1701c <alexkp12355@gmail.com>
2019-05-21 16:28:34 +0200

    Status, connect, connect to specific county and disconnect working

--- src/nordvpn.cpp
+++ src/nordvpn.cpp
@@ -48,7 +48,6 @@ void NordVPN::prepareForMatchSession() {
     process.waitForFinished(-1);
     QByteArray out = process.readAllStandardOutput();
     vpnStatus = out.toStdString();
-    std::cout << "Inited" << std::endl;
 }
 
 
@@ -70,30 +69,45 @@ void NordVPN::match(Plasma::RunnerContext &context) {
     std::stringstream statusStream(vpnStatus);
     while (std::getline(statusStream, statusLine, '\n')) {
         QString l = QString::fromStdString(statusLine);
-        if (l.startsWith("Status: Connect") || l == "Status: Reconnecting") {//Connected / connecting/reconnecting
+        if (l.startsWith("Status: Connect") || l == "Status: Reconnecting") {//Disconnect
             Plasma::QueryMatch disconnectMatch(this);
             disconnectMatch.setIconName(ICON_PATH);
             disconnectMatch.setText(i18n("Disconnect"));
             disconnectMatch.setData("disconnect");
-            disconnectMatch.setRelevance(0);
+            disconnectMatch.setRelevance(0);// TODO increase relevance if term is e.g. vpn d or vpn disconnect
             matches.append(disconnectMatch);
-        } else if (l == "Status: Disconnected") {
+        } else if (l == "Status: Disconnected" && !term.contains("reconnect")) {// Connect
             std::string target = "us";
+            auto split = term.split(' ');
+            if (split.size() > 1) {
+                target = split[1].toStdString();
+            }
             const std::string msg = "Connect to " + target;
             Plasma::QueryMatch connectMatch(this);
             connectMatch.setIconName(ICON_PATH);
-            connectMatch.setText(QString::fromStdString("Connect to ") + QString::fromStdString(target).toUpper());
-            connectMatch.setData(QString::fromStdString("connect;" + target));
+            connectMatch.setText(QString::fromStdString("Connect To ") + QString::fromStdString(target).toUpper());
+            connectMatch.setData(QString::fromStdString("nordvpn connect " + target));
             connectMatch.setRelevance(1);
             matches.append(connectMatch);
         }
-        if (l.startsWith("Status:")) {
+        if (l.startsWith("Status:")) {// Status
             Plasma::QueryMatch statusMatch(this);
             statusMatch.setIconName(ICON_PATH);
             statusMatch.setText(l);
             statusMatch.setData("status");
             statusMatch.setRelevance(0.5);
             matches.append(statusMatch);
+        }
+        if (term.startsWith("vpn reconnect") || term.startsWith("nordvpn reconnect")) {
+            Plasma::QueryMatch statusMatch(this);
+            statusMatch.setIconName(ICON_PATH);
+            statusMatch.setText("Reconnect Test😊");
+            statusMatch.setData("status");
+            statusMatch.setRelevance(0.25);
+            matches.append(statusMatch);
+            // TODO Connect new if disconnected
+            // TODO Reconnect if no spefific value/value == current server
+            // TODO Reconnect to other country/server
         }
 
     }
@@ -106,14 +120,12 @@ void NordVPN::run(const Plasma::RunnerContext &context, const Plasma::QueryMatch
         system(R"(nordvpn d | tr -d '/\-|\\' | xargs -d '\n' notify-send --icon /home/alex/Downloads/ico/nordvpn_favicon57x57.png 2>&1 &)");
     } else if (match.data().toString() == "status") {
         system("status=$(nordvpn status 2>&1 | grep -E 'Status|Current server|Transfer|Your new IP');"
-               "notify-send  \"$status\"  --icon /home/alex/Downloads/ico/nordvpn_favicon57x57.png  2>&1");
-    } else if (match.data().toString().startsWith("connect;")) {
-        QString cmd(
-                R"(nordvpn c <target> | tr -d '/\-|\\' | tail -2 | cut -d '(' -f 1  |xargs -d '\n' notify-send --icon /home/alex/Downloads/ico/nordvpn_favicon57x57.png  2>&1 &)");
-        cmd.replace("<target>", match.data().toString().split(';')[1]);
-        QString exec = "/bin/sh -c \" " + cmd + " \"";
-        std::cout << exec.toStdString() << std::endl;
-        QProcess::startDetached(exec);
+               "notify-send  \"$status\"  --icon /home/alex/Downloads/ico/nordvpn_favicon57x57.png  2>&1 &");
+    } else if (match.data().toString().startsWith("nordvpn connect")) {
+        QString cmd = match.data().toString();
+        cmd += QString(
+                R"( | tr -d '/\-|\\' | tail -2 | cut -d '(' -f 1  |xargs -d '\n' notify-send --icon /home/alex/Downloads/ico/nordvpn_favicon57x57.png 2>&1 &)");
+        system(cmd.toLatin1().constData());
     }
 }
 
